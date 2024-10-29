f7e342c7ae5d6aeb1cd59fc9e687432be6edbc10
LauriCarvalho <00001089442658sp@al.educacao.sp.gov.br>
2024-10-29 15:24:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'Ciência',
+    ' Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    ' Entre 4 a 6 litros. São retirados 450 mililitros.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Amor',
+    'Quem é meu grande amor?',
+    'Marcos'
+)
+
+criaCartao(
+    'Verdade',
+    'Quem é a melhor sala da Oscar?',
+    '3 série do Ensino Médio'
+)
+   
+criaCartao(
+    'Verdade',
+    'Quem é a melhor aluna da Oscar?',
+    'Maria Lauri Carvalho Machado'
+)
+ 
